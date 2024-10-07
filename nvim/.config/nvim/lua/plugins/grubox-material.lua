5511dd43ff4d8ed50936762782d85f7fef4bdc01
Vadim <vadim198527@gmail.com>
2024-10-07 18:18:31 +0000

    arch

--- nvim/.config/nvim/lua/plugins/grubox-material.lua
+++ nvim/.config/nvim/lua/plugins/grubox-material.lua
@@ -6,9 +6,9 @@ return {
         config = function()
             -- Optionally configure and load the colorscheme
             -- directly inside the plugin declaration.
-            vim.g.gruvbox_material_enable_italic = true
+            -- vim.g.gruvbox_material_enable_italic = true
             -- vim.g.gruvbox_material_transparent_background = 0
-            -- vim.cmd.colorscheme('gruvbox-material')
+            vim.cmd.colorscheme('gruvbox-material')
         end
     }
 }
